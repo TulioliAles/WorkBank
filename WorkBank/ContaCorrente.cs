4d053f9e0651f536ecb6b7d594fc2f1ac4422701
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-10-10 20:45:14 -0300

    Inicia o projeto

---
+++ WorkBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkBank
+{
+    public class ContaCorrente
+    {
+        public int NumeroAgencia { get; set; }
+        public string Conta { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+    }
+}
---
+++ WorkBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WorkBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.Titular = "Alessandro";
+            conta.NumeroAgencia = 777;
+            conta.Conta = "020777-X";
+            conta.Saldo = 560;
+            
+            Console.WriteLine($"Saldo = {conta.Saldo}");
+        }
+    }
+}
