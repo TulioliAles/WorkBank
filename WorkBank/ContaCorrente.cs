c5abcddac3579a40e356636f1d093e72c56fe241
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-10-11 20:47:28 -0300

    Criação métodos Sacar, Depositar e Transferir

--- WorkBank/ContaCorrente.cs
+++ WorkBank/ContaCorrente.cs
@@ -6,5 +6,37 @@
         public string Conta { get; set; }
         public string Titular { get; set; }
         public double Saldo { get; set; }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(valor <= this.Saldo)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(valor <= this.Saldo)
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- WorkBank/Program.cs
+++ WorkBank/Program.cs
@@ -6,14 +6,23 @@ namespace WorkBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente();
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.Titular = "Alessandro";
+            conta1.NumeroAgencia = 777;
+            conta1.Conta = "020777-X";
+            conta1.Saldo = 560;            
+            Console.WriteLine($"Saldo conta 1 = {conta1.Saldo}");
 
-            conta.Titular = "Alessandro";
-            conta.NumeroAgencia = 777;
-            conta.Conta = "020777-X";
-            conta.Saldo = 560;
-            
-            Console.WriteLine($"Saldo = {conta.Saldo}");
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.Titular = "Maria";
+            conta2.NumeroAgencia = 17;
+            conta2.Conta = "123336-8";
+            conta2.Saldo = 240;
+            Console.WriteLine($"Saldo conta 2 = {conta2.Saldo}");
+
+            conta1.Transferir(60, conta2);
+            Console.WriteLine($"Saldo conta 1 pós transferência = {conta1.Saldo}");
+            Console.WriteLine($"Saldo conta 2 pós transferência = {conta2.Saldo}");
         }
     }
 }
